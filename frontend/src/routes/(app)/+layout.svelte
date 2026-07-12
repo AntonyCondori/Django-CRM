3efc3bceed25060a76b7e00555162763764c2bdc
SteveArms <acunoc@unsa.edu.pe>
2026-07-12 13:30:54 -0500

    Add ChatBot

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -14,6 +14,19 @@
   });
 </script>
 
+<svelte:head>
+  <script type="module">
+    import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
+    Chatbot.init({
+      chatflowid: "fee610c9-7f9c-4aad-ad63-269fac3b2d48",
+      apiHost: "https://cloud.flowiseai.com",
+      chatflowConfig: {
+        vars: { assistantToken: data.assistantToken }
+      }
+    })
+  </script>
+</svelte:head>
+
 <AppShell user={data.user} org_name={data.org_name} org_settings={data.org_settings}>
   <main class="relative flex-1">
     {@render children()}
